519a833897665561e8fdbe045d223c91aa4b17a6
juliataro <julia.taro@gmail.com>
2021-08-29 10:52:08 +0300

    changed/ array not working

--- src/Ulessanne1/Ulessanne6.java
+++ src/Ulessanne1/Ulessanne6.java
@@ -1,6 +1,6 @@
 package Ulessanne1;
 
-// Julia Taro Task 6
+// Julia Taro Task 6, ne rabotala zapis array
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -9,22 +9,30 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class Ulessanne6 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
 
         int number;
         int sum = 0;
         char choice;
         int amount = 0;
         int average;
+        int i;
 
 
         do {
-            System.out.println("Enter the Number: ");
-            number = scanner.nextInt();
 
+            // Asking number from user
+
+
+            System.out.println("Enter the Number: ");
+            number  = scanner.nextInt();
+
+            // Asking if user agree to continue
             System.out.println("Please enter Y for yes if you want to continue and N if not: ");
             choice = scanner.next().charAt(0);
 
@@ -43,38 +51,12 @@ public class Ulessanne6 {
             average = sum / amount;
 
 
-            // Preparation of Array for saving
-            int[] myArray = new int[amount];
-
-            for (int i = 0; i < number; i++) {
-                myArray[i] = scanner.nextInt();
-            }
-
-
         } while ((choice == 'Y') || (choice == 'y'));
 
         // Display the total sum
         System.out.println("Total sum is: " + sum);
-        System.out.println("Average number is: " + average);
+        System.out.println("Average number is: " + average);// Creating variable for file
 
-
-        for (int i = 0; i < number; i++) ;
-        System.out.println(myArray[i]);
-        // Creating a new file
-
-        try {
-            File myObj = new File("JuliaTaroTask6.txt");
-            if (myObj.createNewFile()) {
-                System.out.println("File created: " + myObj.getName());
-            } else {
-                System.out.println("File already exists.");
-            }
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-
-        // Creating variable for file
 
         File myFail = new File("JuliaTaroTask6.txt");
 
@@ -82,8 +64,8 @@ public class Ulessanne6 {
         try {
             BufferedWriter bw = new BufferedWriter((new FileWriter(myFail)));
 
-            bw.write(sum);
-            bw.write(average);
+            bw.write("Total sum is: " + sum + " ");
+            bw.write("Average number is: " + average);
             bw.close();
             System.out.println("Fail loodud");
         } catch (IOException e) {
